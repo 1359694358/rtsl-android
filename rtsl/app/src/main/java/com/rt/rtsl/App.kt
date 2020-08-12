ef809b52b8ac1ed0b78451d58306c3d6b4b8662b
chin <chin@chinamcloud.com>
2020-08-12 20:18:49 +0800

    初始化

--- rtsl/app/src/main/java/com/rt/rtsl/App.kt
+++ rtsl/app/src/main/java/com/rt/rtsl/App.kt
@@ -2,10 +2,20 @@ package com.rt.rtsl
 
 import androidx.multidex.MultiDexApplication
 import com.rt.rtsl.utils.ExceptionHandler
+import com.tencent.smtt.export.external.TbsCoreSettings
+import com.tencent.smtt.sdk.QbSdk
+import org.jetbrains.anko.doAsync
+
 
 class App: MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
-        ExceptionHandler.getInstance().init(this)
+        doAsync {
+            ExceptionHandler.getInstance().init(this@App)
+            val map = HashMap<String, Any>()
+            map[TbsCoreSettings.TBS_SETTINGS_USE_SPEEDY_CLASSLOADER] = true
+            map[TbsCoreSettings.TBS_SETTINGS_USE_DEXLOADER_SERVICE] = true
+            QbSdk.initTbsSettings(map)
+        }
     }
 }
--- rtsl/app/src/main/java/com/rt/rtsl/utils/Utils.kt
+++ rtsl/app/src/main/java/com/rt/rtsl/utils/Utils.kt
@@ -3,8 +3,13 @@ package com.rt.rtsl.utils
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Parcelable
 import android.util.Log
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebView.setWebContentsDebuggingEnabled
 
 fun Context.startActivity(activity: String) {
     val intent = Intent()
@@ -36,4 +41,56 @@ inline fun <reified T> getParcelableArrayExtra(parcels:  Array<Parcelable> ): Ar
         array[index]=parcelable as T
     }
     return array
+}
+
+fun WebView.setWebSetting() {
+    if (Build.VERSION.SDK_INT >= 17) {
+        getSettings().setMediaPlaybackRequiresUserGesture(false)
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        getSettings().setSafeBrowsingEnabled(false)
+    }
+    if (Build.VERSION.SDK_INT >= 19)
+        setWebContentsDebuggingEnabled(true)
+    getSettings().setDatabaseEnabled(true)
+    setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY)
+    val dir: String =getContext().getDir("database", Context.MODE_PRIVATE).getPath()
+    getSettings().setGeolocationDatabasePath(dir)
+    getSettings().setGeolocationEnabled(true)
+    getSettings().setDomStorageEnabled(true)
+    getSettings().setJavaScriptEnabled(true)
+    getSettings().setAllowFileAccess(true) // 允许访问文件
+    getSettings().setDefaultTextEncodingName("UTF-8")
+    //        getSettings().setAppCacheEnabled(false);
+    getSettings().setSavePassword(false)
+    getSettings().setCacheMode(WebSettings.LOAD_DEFAULT)
+    if (Build.VERSION.SDK_INT >= 21) {
+        settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+    }
+}
+
+fun com.tencent.smtt.sdk.WebView.setWebSetting() {
+    if (Build.VERSION.SDK_INT >= 17) {
+        getSettings().setMediaPlaybackRequiresUserGesture(false)
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//        getSettings().setSafeBrowsingEnabled(false)
+    }
+    if (Build.VERSION.SDK_INT >= 19)
+        com.tencent.smtt.sdk.WebView.setWebContentsDebuggingEnabled(true)
+    getSettings().setDatabaseEnabled(true)
+    setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY)
+    val dir: String =getContext().getDir("database", Context.MODE_PRIVATE).getPath()
+    getSettings().setGeolocationDatabasePath(dir)
+    getSettings().setGeolocationEnabled(true)
+    getSettings().setDomStorageEnabled(true)
+    getSettings().setJavaScriptEnabled(true)
+    getSettings().setAllowFileAccess(true) // 允许访问文件
+    getSettings().setDefaultTextEncodingName("UTF-8")
+    //        getSettings().setAppCacheEnabled(false);
+    getSettings().setSavePassword(false)
+    getSettings().setCacheMode(WebSettings.LOAD_DEFAULT)
+    if (Build.VERSION.SDK_INT >= 21) {
+        settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+    }
 }
