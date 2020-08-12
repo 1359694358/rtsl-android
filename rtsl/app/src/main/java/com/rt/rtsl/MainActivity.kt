fa2178d56394a3d917aa24def76bcba46c975314
chin <chin@chinamcloud.com>
2020-08-12 10:13:50 +0800

    首页

--- rtsl/app/src/main/java/com/rt/rtsl/MainActivity.kt
+++ rtsl/app/src/main/java/com/rt/rtsl/MainActivity.kt
@@ -2,11 +2,13 @@ package com.rt.rtsl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rt.rtsl.utils.StatusBarUtil
 import com.rtsl.app.android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        StatusBarUtil.setLightMode(this)
     }
 }
